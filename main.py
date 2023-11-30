9075d5bb260803038ffd008a502367e7f29b8d36
Luis Antonio Dominguez <ladominguez@ucla.edu>
2023-11-30 10:24:19 -0600

    Reading model from file

--- main.py
+++ main.py
@@ -3,16 +3,8 @@ from utils import *
 if __name__ == "__main__":
     depth = 12
     station_distance =17
-    for theta in np.linspace(0, 70, 10):
-        xi, distances = estimate_distances(theta, depth)
-        print('theta = %5.3f xi = %5.3f '% (theta, xi))
 
-
-    #theta = solve_theta(station_distance, depth)
-    #print('theta = ', theta)
-    #xi, distances = estimate_distances(theta, depth)
-    #print('xi = ', xi)
-    #print('distances = ', distances)    
+    thickness, velocities = np.loadtxt('model.txt', unpack=True, skiprows=1)
     fig = plot_model(max_dist=station_distance + 10)
     fig = plot_solution(fig, depth, station_distance)
     fig.savefig('model.png')
